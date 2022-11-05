95c39f1e9dcce4fda8026a37612609d0c3303dfb
Tobias Castañeda <tobias.2000david@gmail.com>
2022-11-05 16:45:17 -0300

    Subo header, container, toolbar, menu y MainActivityPrueba para mostrar menu lateral (sin comportamiento aun)

---
+++ app/src/main/java/xyz/dev3k/ateneo2/MainActivityPrueba.java
@@ -0,0 +1,58 @@
+package xyz.dev3k.ateneo2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivityPrueba extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_prueba);
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView_id);
+        toolbar = findViewById(R.id.toolbar_id);
+
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+
+        ActionBarDrawerToggle actionBarDrawerToggle =
+                new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.drawer_close,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+}

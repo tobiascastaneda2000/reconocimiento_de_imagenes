db16642716e1ec5a8e8243e74565f15ba0c7f27b
Tobias Castañeda <tobias.2000david@gmail.com>
2022-11-15 13:47:14 -0300

    Agrego codigo a Camera fragmento

--- app/src/main/java/xyz/dev3k/ateneo2/Fragments/CameraFragment.java
+++ app/src/main/java/xyz/dev3k/ateneo2/Fragments/CameraFragment.java
@@ -1,22 +1,283 @@
 package xyz.dev3k.ateneo2.Fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+
+import xyz.dev3k.ateneo2.AnalysisActivity;
+import xyz.dev3k.ateneo2.CaptureActivity;
 import xyz.dev3k.ateneo2.R;
 
-public class CameraFragment extends Fragment {
+public class CameraFragment extends Fragment implements View.OnClickListener{
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView previewView;
+    Button imageCaptureButton, videoCaptureButton, fileButton;
+    private ImageCapture imageCapture;
+    private VideoCapture videoCapture;
+    private ImageAnalysis imageAnalysis;
+    ImageAnalysis.Analyzer analizer;
+    private String TAG;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        //setContentView(R.layout.activity_capture);
+        imageCaptureButton = view.findViewById(R.id.capture_button);
+        videoCaptureButton = view.findViewById(R.id.video_capture_button);
+        fileButton = view.findViewById(R.id.btn_file);
+        previewView = view.findViewById(R.id.viewFinder_id);
+
+        imageCaptureButton.setOnClickListener(this);
+        videoCaptureButton.setOnClickListener(this);
+        fileButton.setOnClickListener(this);
+
+        //cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this.getContext());
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                starCameraX(cameraProvider);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getExecutor());
+
+        return view;
+    }
+
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this.getContext());
+        //return ContextCompat.getMainExecutor(this.getContext());
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void starCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        //Camera selector use case
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        //Preview use case
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        //Image capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        //Video capture use case
+        videoCapture = new VideoCapture.Builder()
+                .setVideoFrameRate(30)
+                .build();
+
+        //Analisys use case
+        imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture/*, videoCapture*/, imageAnalysis);
+    }
+
+    //Switch onClick
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.image_capture_button:
+                capturePhoto();
+                //launchAnalysisActivity();
+                break;
+            case R.id.video_capture_button:
+                if (videoCaptureButton.getText() == "grabar") {
+                    videoCaptureButton.setText("parar");
+                    recordVideo();
+                } else {
+                    videoCaptureButton.setText("grabar");
+                    videoCapture.stopRecording();
+                }
+                break;
+            case R.id.btn_file:
+                //launchAnalysisActivity(view);
+        }
+    }
+
+    private void capturePhoto() {
+
+        long timestamp = System.currentTimeMillis();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+
+        imageCapture.takePicture(
+                new ImageCapture.OutputFileOptions.Builder(
+                        getContext().getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues
+                ).build(),
+                getExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        Toast.makeText(CameraFragment.this.getContext(), "Foto guardada", Toast.LENGTH_SHORT).show();
+                        Uri uri = outputFileResults.getSavedUri();
+                        launchAnalysisActivity(uri);
+                    }
+
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Toast.makeText(CameraFragment.this.getContext(), "Error al guardar foto: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    private void launchAnalysisActivity(Uri uri) {
+/*
+        Intent intent = new Intent(this, AnalysisActivity.class);
+        intent.putExtra("image", uri);
+        startActivity(intent);*/
+
+        /*BUscar forma de pasar metadatos entre fragmentos*/
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.container_id, new AnalisisFragment());
+        fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
+
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void recordVideo() {
+        if (videoCapture != null) {
+            long timestamp = System.currentTimeMillis();
+
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timestamp);
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
+            if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            videoCapture.startRecording(
+                    new VideoCapture.OutputFileOptions.Builder(
+                            getContext().getContentResolver(),
+                            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                            contentValues
+                    ).build(),
+                    getExecutor(),
+                    new VideoCapture.OnVideoSavedCallback() {
+                        @Override
+                        public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+                            Toast.makeText(CameraFragment.this.getContext(), "Video guardado", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                            Toast.makeText(CameraFragment.this.getContext(), "Error al guardar el video: " + message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+            );
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    public void analyze(ImageProxy imageProxy) {
+        @SuppressLint("UnsafeOptInUsageError") Image mediaImage = imageProxy.getImage();
+        if (mediaImage != null) {
+            InputImage imageInput =
+                    InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+            // Pass image to an ML Kit Vision API
+            ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+
+            labeler.process(imageInput)
+                    .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                        @Override
+                        public void onSuccess(List<ImageLabel> labels) {
+                            Toast.makeText(CameraFragment.this.getContext(), "Etiquetas:", Toast.LENGTH_SHORT).show();
+                            for (ImageLabel label : labels) {
+                                String text = label.getText();
+                                float confidence = label.getConfidence();
+                                int index = label.getIndex();
+                                Toast.makeText(CameraFragment.this.getContext(), text, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(CameraFragment.this.getContext(), "Error, no se etiquetó la imagen", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 }
